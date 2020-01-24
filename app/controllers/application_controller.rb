f62ab67fa22d21879e742276224085f53fce0e3c
mr.beerdy <mr.beerdy@gmail.com>
2020-01-24 13:57:28 +0300

    telgramm and ehatsapp to base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,11 @@ class ApplicationController < ActionController::Base
 
     Content.all.each do |content|
       @catalog_xls_url = content.asset.url if content.url == 'catalog_xls_url'
+      
+      @whatsapp_link_1 = content.link if content.url == 'whatsapp_link_1'
+      @telegram_link_1 = content.link if content.url == 'telegram_link_1'
+      @whatsapp_link_2 = content.link if content.url == 'whatsapp_link_2'
+      @telegram_link_2 = content.link if content.url == 'telegram_link_2'
     end
 
   end
