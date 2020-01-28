3e00d9aeff68c806745adf2cb2739ebc9132f9b0
mr.beerdy <mr.beerdy@gmail.com>
2020-01-28 16:44:13 +0300

    logo to admin

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,7 +2,7 @@ class Content < ApplicationRecord
   has_attached_file :asset
   
   has_attached_file :asset, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :asset, :content_type => ["application/pdf","application/vnd.ms-excel",     
+  validates_attachment_content_type :asset, :content_type => [/\Aimage\/.*\z/,"application/pdf","application/vnd.ms-excel",     
     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     "application/msword", 
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document", 
